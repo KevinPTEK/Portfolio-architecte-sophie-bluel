bb74779ea41266ba838e874398c7accb927e9a90
Kevin <renou.kevin@icloud.com>
2026-05-21 19:55:43 +0200

    refacte boucle for function renderBtnFilter

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,28 +1,4 @@
-// 1ere version :
-// const gallery = document.querySelector(".gallery")
 
-// async function loadWorks() {
-//     const reponse = await fetch("http://localhost:5678/api/works")
-//     const works = await reponse.json()
-
-//     for (let i = 0; i < works.length; i++) {
-//         const work = works[i]
-
-//         const workCard = document.createElement("figure")
-//         const imgWorkCard = document.createElement("img")
-//         const legendWorkCard = document.createElement("figcaption")
-
-//         imgWorkCard.src = work.imageUrl
-//         imgWorkCard.alt = work.title
-//         legendWorkCard.innerText = work.title
-
-//         workCard.appendChild(imgWorkCard)
-//         workCard.appendChild(legendWorkCard)
-//         gallery.appendChild(workCard)
-//     }
-// }
-
-// loadWorks()
 
 // 2eme Version 
 // variable global 
@@ -71,17 +47,21 @@ function renderBtnFilter () {
     
     //Création du bouton "Tous"
     const btnAll = document.createElement("button")
-    btnAll.classList.add("filters__btn")
+    btnAll.classList.add("filters__btn", "filters__btn--active")
+    btnAll.dataset.categoriesId = ("all")
     btnAll.innerText = ("Tous")
     filters.appendChild(btnAll)
+    console.log(btnAll)
 
-    for (let i = 0; i < categories.length; i++) {
-        const categorie = categories[i]
+    for (const categorie of categories) {
 
         const btn = document.createElement("button")
         btn.classList.add("filters__btn")
         btn.innerText = categorie.name
+        btn.dataset.categoriesId = categorie.id
         filters.appendChild(btn)
+
+        console.log(btn)
     }
     
 }
