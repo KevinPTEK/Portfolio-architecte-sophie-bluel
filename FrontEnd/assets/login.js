7bbc92a51eb2552c5dc923ed18cec56302fe55d2
Kevin <renou.kevin@icloud.com>
2026-05-25 19:56:02 +0200

    mise en place authentification avec sauvegarde token

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -2,7 +2,7 @@
 export function ajoutListenerEnvoyerLogIn() {
     const form = document.querySelector(".logIn__username")
     
-    form.addEventListener("submit", function(event) {
+    form.addEventListener("submit", async function(event) {
         event.preventDefault()
         const log = {
             email: event.target.querySelector("[name=email").value,
@@ -11,14 +11,21 @@ export function ajoutListenerEnvoyerLogIn() {
         }
         console.log(log)
         const sendLogIn = JSON.stringify(log);
-        fetch("http://localhost:5678/api/users/login", {
+        const reponse = await fetch("http://localhost:5678/api/users/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: sendLogIn
         });
+
+        let token = await reponse.json()
+        let valueToken = JSON.stringify(token.token);
+        window.localStorage.setItem("token", valueToken);
         console.log(sendLogIn)
+        console.log(token)
         
     })
 }
 
+
 ajoutListenerEnvoyerLogIn()
+
